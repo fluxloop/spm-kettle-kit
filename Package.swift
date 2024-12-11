7cfd278ba6d16630e3eedbe8fe680712f0c757a5
Kissor Jeyabalan <kij@kij.no>
2024-12-11 10:11:47 +0100

    fix: invalid manifest, 1.4.0

--- Package.swift
+++ Package.swift
@@ -1,22 +1,19 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let Package = (
+let package = Package(
     name: "KettleKit",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12),
     ],
     products: [
-        .library(
-	    name: "KettleKit",
-	    targets: ["KettleKit"]
-	)
+        .library(name: "KettleKit", targets: ["KettleKit"]),
     ],
     targets: [
         .binaryTarget(
             name: "KettleKit",
-	    url: "https://kettleartifacts.blob.core.windows.net/ios/1.4.0.zip",
-	    checksum: "4731e965153b46957bba4046604d3b18c08854180fd4a8067b4113c217aa8671"
-	)
+            url: "https://kettleartifacts.blob.core.windows.net/ios/1.4.0.zip",
+            checksum: "4731e965153b46957bba4046604d3b18c08854180fd4a8067b4113c217aa8671"
+        ),
     ]
 )
